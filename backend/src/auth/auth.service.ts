62f41a5a7d5ba8f4001a8b8a88872a7f159f4584
Rifki Rahadian <rrahadian94@gmail.com>
2024-06-12 20:54:07 +0700

    feat: backend login

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,52 @@
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+
+  async findOne(username: string): Promise<User> {
+    const findUser = this.users.find((user) => user.username === username);
+    if (!findUser) {
+      throw new NotFoundException('User not found');
+    }
+
+    const user = new User();
+    user.userId = findUser.userId;
+    user.username = findUser.username;
+    user.password = findUser.password;
+
+    return user;
+  }
+
+  async signIn(
+    username: string,
+    pass: string,
+  ): Promise<{ access_token: string }> {
+    const user = await this.findOne(username);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const payload = { sub: user.userId, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
---
+++ backend/src/auth/entities/user.entity.ts
@@ -0,0 +1,7 @@
+export class User {
+  userId: number;
+
+  username: string;
+
+  password: string;
+}
